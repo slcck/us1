0c95f5700e7a81102d627fa236e37fa8c6137014
slcck <101538261+slcck@users.noreply.github.com>
2022-10-13 13:50:12 +1100

    Add files via upload

---
+++ devil-v2ray.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#Config v2ray
+
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+#run v2ray server
+
+xray -c /etc/xray/config.json
